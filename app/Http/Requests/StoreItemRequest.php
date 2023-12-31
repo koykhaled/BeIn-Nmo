4fbbdfb3f5c07f1e5a77111c3a42ebd45571a476
koykhaled <koykhaled@gmail.com>
2023-12-31 09:57:25 +0300

    Item endpoints (routes + controller + request)

--- app/Http/Requests/StoreItemRequest.php
+++ app/Http/Requests/StoreItemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreCategoryRequest extends FormRequest
+class StoreItemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,10 @@ class StoreCategoryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
-            'name' => "required|string",
-            'parent_id' => 'nullable',
+            'name' => 'required|string',
+            'price' => 'required|numeric',
+            'discount_price' => 'nullable|numeric',
+            'category_id' => 'required|exists:categories,id',
         ];
     }
 }
